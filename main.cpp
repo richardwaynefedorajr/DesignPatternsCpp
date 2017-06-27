ef4c661f50c594211997d63af84a82088e612d8c
Rich <rfedora1@vt.edu>
2017-06-27 16:11:07 -0400

    Created main()

---
+++ main.cpp
@@ -0,0 +1,11 @@
+# include <iostream>
+# include <stdio.h>
+# include <vector>
+
+int main(int argc, char *argv[]) {
+
+    std::cout << "Main point of entry for use of Gang of Four design pattern implementations" << std::endl;
+
+    return 0;
+
+}
