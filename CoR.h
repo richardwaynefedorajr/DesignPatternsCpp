f3c89a4cb9e1d7112abd2018cb9eee6ad2052bb7
Rich <rfedora1@vt.edu>
2017-07-29 15:57:59 -0400

    Declared classes for chain of responsibility design pattern in CoR.h

--- CoR.h
+++ CoR.h
@@ -1,1 +1,28 @@
+# include <string>
+# include <iostream>
 
+enum FastFood { burger, fries, drink, change };
+
+class BaseWorker {
+    BaseWorker *m_next_worker;
+    public:
+        BaseWorker();
+        void setNext(BaseWorker *in_worker);
+        void addWorker(BaseWorker *in_worker);
+        virtual void service(FastFood in_request);
+};
+
+class Cashier {
+    public:
+        void service(FastFood in_request);
+};
+
+class Cook {
+    public:
+        void service(FastFood in_request);
+};
+
+class Runner {
+    public:
+        void service(FastFood in_request);
+};
